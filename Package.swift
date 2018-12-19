b0df7ce80b7bc4c54a0278e8f1dce89ce786a01c
fanquake <fanquake@gmail.com>
2018-12-19 09:16:32 +0800

    Add PromiseKit & PromiseKit/Foundations packages

--- Package.swift
+++ Package.swift
@@ -7,8 +7,12 @@ let package = Package(
     products: [
         .library(name: "CoreRPC", targets: ["CoreRPC"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/mxcl/PromiseKit", from: "6.7.1"),
+        .package(url: "https://github.com/PromiseKit/Foundation", from: "3.3.0")
+    ],
     targets: [
-        .target(name: "CoreRPC"),
+        .target(name: "CoreRPC", dependencies: ["PromiseKit", "PMKFoundation"]),
         .testTarget(name: "CoreRPCTests", dependencies: ["CoreRPC"]),
     ]
 )
