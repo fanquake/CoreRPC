27a5c3b3ce083bbde9bc7fd7c4c3321c904c7fa1
fanquake <fanquake@gmail.com>
2019-01-27 18:07:59 +0800

    network: add Name enum to Network struct

--- Sources/CoreRPC/Network.swift
+++ Sources/CoreRPC/Network.swift
@@ -2,32 +2,40 @@ import Foundation
 import PromiseKit
 
 public extension CoreRPC {
+
     func getNetworkInfo() -> Promise<NetworkInfo> {
         return call(method: .getnetworkinfo, params: Empty())
     }
     
-    struct Network: Codable {
+    struct Network: Decodable {
+
+        public enum Name: String, Decodable {
+            case ipv4
+            case ipv6
+            case onion
+        }
+
         public let limited: Bool
-        public let name: String
+        public let name: Name
         public let proxy: String
         public let proxy_randomize_credentials: Bool
         public let reachable: Bool
     }
     
-    struct NetworkAddress: Codable {
+    struct NetworkAddress: Decodable {
         public let address: String
         public let port: Int
         public let score: Int
     }
     
-    struct NetworkInfo: Codable {
+    struct NetworkInfo: Decodable {
         public let connections: Int
         public let incrementalfee: Double
-        public let localaddresses: Array<NetworkAddress>
-        public let networkactive: Bool
-        public let networks: Array<Network>
+        public let localaddresses: [NetworkAddress]
         public let localrelay: Bool
         public let localservices: String
+        public let networkactive: Bool
+        public let networks: [Network]
         public let protocolversion: Int
         public let relayfee: Double
         public let subversion: String
